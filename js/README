eab5a192e56d613ee7e9cb8d91e55149d661c8ff
Teodora Aldridge <teodora.aldridge@bjss.com>
2023-09-22 16:53:01 +0100

    add files

---
+++ js/README
@@ -0,0 +1,5 @@
+This repo was created, following these two tutorials on Jest basics:
+
+Introduction To Testing In JavaScript With Jest - https://www.youtube.com/watch?v=FgnxcUQ5vho
+
+Jest Crash Course - Unit Testing in JavaScript - https://www.youtube.com/watch?v=7r4xVDI2vho
---
+++ js/anagram.js
@@ -0,0 +1,17 @@
+const { formatStackTrace } = require("jest-message-util");
+
+function isAnagram(str1, str2) {
+    return formatStr(str1) === formatStr(str2);
+}
+
+//Helper function
+function formatStr(str) {
+    return str
+        .replace(/[^\w]/g, '')
+        .toLowerCase()
+        .split('')
+        .sort()
+        .join('')
+}
+
+module.exports = isAnagram;
